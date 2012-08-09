824c9dbaff62cf507ae67fcd7295b05145b8f7e9
hnth2507@gmail.com <hnth2507@gmail.com@86b71d34-fcdf-67f1-f518-776054f02563>
2012-08-09 06:27:57 +0000

    add folder define

---
+++ trunk/TheBallRunner/src/Define/Const.h
@@ -0,0 +1,20 @@
+//
+//  Const.h
+//  TheBallRunner
+//
+
+
+//============================== DEFINITION ===============================//
+
+#define PLAYER_TAG 101
+#define OBSTACLE_TAG 202
+#define WALL_TAG 303
+
+enum EnumObjectTag 
+{
+    EnumObjPlayerTag, 
+    EnumObjOsbtacleTag,
+    EnumObjCubeTag,
+    EnumObjSphereTag,
+    
+};
